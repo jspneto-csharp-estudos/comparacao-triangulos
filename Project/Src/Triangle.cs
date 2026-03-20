2e048e4c77c2e58b23e6d0e7a59104297303414e
José de Senna <josespnt1990@gmail.com>
2026-03-20 15:36:43 -0300

    refactor: adapta a classe Triangle para utilizar Segment na representacao dos lados
    - ExtractNameSides(): retorna um vetor de strings que representam os nomes dos lados
    - NameIsValid(): torna-se publico para ser utilizado em TriangleInputs e ProgramExamples
    - Area(): adaptado para utilizar o comprimento dos lados do segmento
    - ToString(): adaptado para utilizar a representacao textual dos segmentos
    - construtor e setter  garantem que o nome do triangulo esteja sempre em caixa alta
    - remove metodo obsoleto ValidateSide() (a validacao passa a ser feita pelo segmento)

--- Project/Src/Triangle.cs
+++ Project/Src/Triangle.cs
@@ -5,23 +5,25 @@ namespace Project.Src;
 class Triangle
 {
     private string _name;
-    public double S1 { get; private set; }
-    public double S2 { get; private set; }
-    public double S3 { get; private set; }
+    public Segment S1 { get; private set; }
+    public Segment S2 { get; private set; }
+    public Segment S3 { get; private set; }
 
     public Triangle(string? name, double? s1, double? s2, double? s3)
     {
-        _name = NameIsValid(name) ? name! : "ABC";
-        S1 = ValidateSide(s1);
-        S2 = ValidateSide(s2);
-        S3 = ValidateSide(s3);
+        _name = NameIsValid(name) ? name!.ToUpper() : "ABC";
+        
+        string[] sideNames = ExtractNameSides(_name);
+        S1 = new(sideNames[0], s1);
+        S2 = new(sideNames[1], s2);
+        S3 = new(sideNames[2], s3);
 
-        if (!TriangleInequality(S1, S2, S3))
+        if (!TriangleInequality(S1.Length, S2.Length, S3.Length))
         {
-            S1 = 3.0;
-            S2 = 4.0;
-            S3 = 5.0;
-        }        
+            S1 = new(sideNames[0], 3.0);
+            S2 = new(sideNames[1], 4.0);
+            S3 = new(sideNames[2], 5.0);
+        }
     }
 
     public Triangle(string? name) : this(name, 3.0, 4.0, 5.0) {}
@@ -37,19 +39,14 @@ class Triangle
         {
             if (NameIsValid(value))
             {
-                _name = value!;
+                _name = value!.ToUpper();
+
+                string[] sideNames = ExtractNameSides(_name);
+                S1 = new(sideNames[0], S1.Length);
+                S2 = new(sideNames[1], S2.Length);
+                S3 = new(sideNames[2], S3.Length);
             }
         }
-    }
-
-    private static double ValidateSide(double? length)
-    {
-        return length.HasValue && length.Value >= 0.0 ? length.Value : 1.0;
-    }
-
-    private static bool NameIsValid(string? name)
-    {
-        return !string.IsNullOrWhiteSpace(name) && name.Length == 3;
     }
 
     private static bool TriangleInequality(double s1, double s2, double s3)
@@ -57,19 +54,33 @@ class Triangle
         return s1 < s2 + s3 && s2 < s1 + s3 && s3 < s1 + s2;
     }
 
+    public static string[] ExtractNameSides(string triangleName)
+    {
+        return [
+            $"{triangleName[0]}{triangleName[1]}",
+            $"{triangleName[0]}{triangleName[2]}",
+            $"{triangleName[1]}{triangleName[2]}"
+        ];
+    }
+
+    public static bool NameIsValid(string? name)
+    {
+        return !string.IsNullOrWhiteSpace(name) && name.Length == 3;
+    }
+
     public double Area()
     {
-        double p = (S1 + S2 + S3) / 2.0;
-        return Math.Sqrt(p * (p - S1) * (p - S2) * (p - S3));
+        double p = (S1.Length + S2.Length + S3.Length) / 2.0;
+        return Math.Sqrt(p * (p - S1.Length) * (p - S2.Length) * (p - S3.Length));
     }
 
     public override string ToString()
     {
         return $"{Name}"
             + $"({Area().ToString("F4", CultureInfo.InvariantCulture)}), "
-            + $"{S1.ToString("F4", CultureInfo.InvariantCulture)}, "
-            + $"{S2.ToString("F4", CultureInfo.InvariantCulture)}, "
-            + $"{S3.ToString("F4", CultureInfo.InvariantCulture)}";
+            + $"{S1.ToString()}, "
+            + $"{S2.ToString()}, "
+            + $"{S3.ToString()}";
             
     }
 }
