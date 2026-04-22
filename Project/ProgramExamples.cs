4c21c612ab5bc10e857c5630c13a90ceeb908bc4
José de Senna <josespnt1990@gmail.com>
2026-04-22 15:43:58 -0300

    refactor: adapta ProblemSolution para chamar a classe Triangle

--- Project/ProgramExamples.cs
+++ Project/ProgramExamples.cs
@@ -1,4 +1,5 @@
 using System.Globalization;
+using Project.Src;
 
 namespace Project;
 
@@ -6,35 +7,37 @@ class ProgramExamples
 {
     public static void ProgramSolution()
     {
-        Console.WriteLine("Entre com as medidas dos lados do triângulo X:");
-        double xA = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
-        double xB = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
-        double xC = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        Triangle t1 = new();
+        Triangle t2 = new();
+        
+        Console.WriteLine("Entre com os dados do 1º triângulo:");
+        t1.Name = Console.ReadLine()!;
+        t1.Side1 = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        t1.Side2 = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        t1.Side3 = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
         Console.WriteLine();
-        Console.WriteLine("Entre com as medidas dos lados do triângulo Y:");
-        double yA = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
-        double yB = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
-        double yC = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        Console.WriteLine("Entre com os dados do 2º triângulo:");
+        t2.Name = Console.ReadLine()!;
+        t2.Side1 = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        t2.Side2 = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        t2.Side3 = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
-        double xP = (xA + xB + xC) / 2.0;
-        double yP = (yA + yB + yC) / 2.0;
-
-        double xArea = Math.Sqrt(xP * (xP - xA) * (xP - xB ) * (xP - xC));
-        double yArea = Math.Sqrt(yP * (yP - yA) * (yP - yB ) * (yP - yC));
+        double areaT1 = t1.Area();
+        double areaT2 = t2.Area();
 
         Console.WriteLine();
-        Console.WriteLine("Área de X: " + xArea.ToString("F4", CultureInfo.InvariantCulture));
-        Console.WriteLine("Área de Y: " + yArea.ToString("F4", CultureInfo.InvariantCulture));
+        Console.WriteLine($"Área de {t1.Name}: {areaT1.ToString("F4", CultureInfo.InvariantCulture)}");
+        Console.WriteLine($"Área de {t2.Name}: {areaT2.ToString("F4", CultureInfo.InvariantCulture)}");
         Console.Write("Maior área: ");
 
-        if (xArea >= yArea)
+        if (areaT1 >= areaT2)
         {
-            Console.WriteLine("X");
+            Console.WriteLine($"{t1.Name}");
         }
         else
         {
-            Console.WriteLine("Y");
+            Console.WriteLine($"{t2.Name}");
         }
 
         ShowExitPrompt();
