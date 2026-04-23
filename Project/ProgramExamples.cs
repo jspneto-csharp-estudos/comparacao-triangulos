18415270b766e6aad99e36352584536a1bf9127e
José de Senna <josespnt1990@gmail.com>
2026-04-23 13:50:26 -0300

    refactor: adapta ProblemSolution para usar classes auxiliares

--- Project/ProgramExamples.cs
+++ Project/ProgramExamples.cs
@@ -1,5 +1,6 @@
-using System.Globalization;
 using Project.Src;
+using Project.Src.IO;
+using Project.Src.Utils;
 
 namespace Project;
 
@@ -7,38 +8,19 @@ class ProgramExamples
 {
     public static void ProgramSolution()
     {
-        Triangle t1 = new();
-        Triangle t2 = new();
-        
         Console.WriteLine("Entre com os dados do 1º triângulo:");
-        t1.Name = Console.ReadLine()!;
-        t1.Side1 = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
-        t1.Side2 = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
-        t1.Side3 = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        Triangle t1 = TriangleInputs.ReadTriangle();
 
         Console.WriteLine();
         Console.WriteLine("Entre com os dados do 2º triângulo:");
-        t2.Name = Console.ReadLine()!;
-        t2.Side1 = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
-        t2.Side2 = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
-        t2.Side3 = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        Triangle t2 = TriangleInputs.ReadTriangle();
 
-        double areaT1 = t1.Area();
-        double areaT2 = t2.Area();
+        Triangle largestArea = TriangleCompare.GetLargestAreaTriangle(t1, t2);
 
         Console.WriteLine();
-        Console.WriteLine($"Área de {t1.Name}: {areaT1.ToString("F4", CultureInfo.InvariantCulture)}");
-        Console.WriteLine($"Área de {t2.Name}: {areaT2.ToString("F4", CultureInfo.InvariantCulture)}");
-        Console.Write("Maior área: ");
-
-        if (areaT1 >= areaT2)
-        {
-            Console.WriteLine($"{t1.Name}");
-        }
-        else
-        {
-            Console.WriteLine($"{t2.Name}");
-        }
+        TriangleOutputs.ShowArea(t1);
+        TriangleOutputs.ShowArea(t2);
+        Console.WriteLine($"Maior área: {largestArea.Name}");
 
         ShowExitPrompt();
     }
